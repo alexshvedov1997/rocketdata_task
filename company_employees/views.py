c063c8e2cb99fa029a1b3cd19539d665adb82209
Alex Shvedov <alexandershvedov1997@gmail.com>
2021-07-22 20:19:19 +0300

    Add permissions

--- company_employees/views.py
+++ company_employees/views.py
@@ -2,12 +2,14 @@ from django.shortcuts import render
 from rest_framework import generics
 from .serializers import EmployerListSerializer
 from .models import Employer
+from .permissions import IsOwnerOrReadOnly
 
 # Create your views here.
 
 class EmployerListView(generics.ListAPIView):
     serializer_class = EmployerListSerializer
     queryset = Employer.objects.all()
+    permission_classes = (IsOwnerOrReadOnly, )
 
 
 class EmployerLevelListView(generics.ListAPIView):
