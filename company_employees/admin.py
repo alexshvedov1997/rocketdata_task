9e68c8ad4acd108044230696f950fe0b5896ddf8
Alex Shvedov <alexandershvedov1997@gmail.com>
2021-07-24 11:33:07 +0300

    Add celery task to clear salary

--- company_employees/admin.py
+++ company_employees/admin.py
@@ -1,11 +1,18 @@
 from django.contrib import admin
 import decimal
 from .models import Position, Level, OtherInformation, Chief, Employer
+from .tasks import clear_salary_task
 
 # Register your models here.
 
 def clear_salary(modeladmin, request, queryset):
-    queryset.update(paid_salary=decimal.Decimal('0'))
+    if len(queryset) > 20:
+        lst_id = []
+        for empl in queryset:
+            lst_id.append(empl.id)
+        clear_salary_task.delay(lst_id)
+    else:
+        queryset.update(paid_salary=decimal.Decimal('0'))
 clear_salary.short_description = "Очистить зарплату"
 
 @admin.register(Position)
@@ -29,10 +36,12 @@ class ChiefAdmin(admin.ModelAdmin):
 
 @admin.register(Employer)
 class EmployerAdmin(admin.ModelAdmin):
-    list_display = ["full_name", "position", "level", "chief", "salary", "paid_salary"]
+    list_display = ["id", "full_name", "position",
+                    "level", "chief", "salary", "paid_salary"]
     list_display_links = ["chief"]
     list_filter = ["level", "position"]
-    actions = [clear_salary,]
+    ordering = ("id",)
+    actions = [clear_salary, ]
 
 
 
--- company_employees/tasks.py
+++ company_employees/tasks.py
@@ -2,14 +2,23 @@ from celery import shared_task
 from celery.task import periodic_task
 from celery.schedules import crontab
 from .models import Employer
+import decimal
 
 
 @periodic_task(run_every=(crontab(minute='*/5')), name='add_salary_empl')
 def add_salary_empl():
     obj_empl = Employer.objects.get(id=1)
-    obj_empl.paid_salary=obj_empl.paid_salary+obj_empl.salary
+    obj_empl.paid_salary = obj_empl.paid_salary+obj_empl.salary
     obj_empl.save()
 
+
+@shared_task
+def clear_salary_task(lst_id):
+    print("Celery clear salary task start")
+    for empl_id in lst_id:
+        empl = Employer.objects.get(id=empl_id)
+        empl.paid_salary = decimal.Decimal('0')
+        empl.save()
 
 
 
